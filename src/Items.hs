fa300e2e9b4f3ee4c49cce9dac7cd3d60bc523fb
Shaun Sharples <shaun.sharples@gmail.com>
2025-03-08 22:30:03 +0200

    Enhance item system with type, adjectives, and improved matching

--- src/Items.hs
+++ src/Items.hs
@@ -1,26 +1,89 @@
 module Items (
   Item (..),
+  ItemType (..),
+  Adjective (..),
   getItemName,
   getItemDescription,
+  getItemType,
+  getItemAdjectives,
+  matchItem,
 ) where
 
+import Data.Char (toLower)
+import Data.List (find)
 import Data.Map (Map)
-import qualified Data.Map as Map
+import Data.Map qualified as Map
+import Data.Maybe (mapMaybe)
+import Data.Set (Set)
+import Data.Set qualified as Set
 
--- Item type
-data Item
-  = RustySword
-  | BrassLantern
-  | WaterFlask
+-- Base item types
+data ItemType
+  = Sword
+  | Lantern
+  | Flask
   deriving (Show, Eq, Ord)
 
--- Item names
-itemNames :: Map Item String
-itemNames =
+-- Adjectives for items
+data Adjective
+  = Rusty
+  | Silver
+  | Brass
+  | Metal
+  | Water
+  | Empty
+  deriving (Show, Eq, Ord)
+
+-- Full item type with descriptor
+data Item
+  = RustySword -- A rusty, metal sword
+  | SilverSword -- A silver, metal sword
+  | BrassLantern -- A brass, metal lantern
+  | WaterFlask -- A water flask
+  | EmptyFlask -- An empty flask
+  deriving (Show, Eq, Ord)
+
+-- Map items to their base type
+itemTypes :: Map Item ItemType
+itemTypes =
   Map.fromList
-    [ (RustySword, "rusty sword")
-    , (BrassLantern, "brass lantern")
-    , (WaterFlask, "water flask")
+    [ (RustySword, Sword)
+    , (SilverSword, Sword)
+    , (BrassLantern, Lantern)
+    , (WaterFlask, Flask)
+    , (EmptyFlask, Flask)
+    ]
+
+-- Map items to their adjectives
+itemAdjectives :: Map Item (Set Adjective)
+itemAdjectives =
+  Map.fromList
+    [ (RustySword, Set.fromList [Rusty, Metal])
+    , (SilverSword, Set.fromList [Silver, Metal])
+    , (BrassLantern, Set.fromList [Brass, Metal])
+    , (WaterFlask, Set.fromList [Water])
+    , (EmptyFlask, Set.fromList [Empty])
+    ]
+
+-- Base type names
+typeNames :: Map ItemType String
+typeNames =
+  Map.fromList
+    [ (Sword, "sword")
+    , (Lantern, "lantern")
+    , (Flask, "flask")
+    ]
+
+-- Adjective names (for display and matching)
+adjectiveNames :: Map Adjective String
+adjectiveNames =
+  Map.fromList
+    [ (Rusty, "rusty")
+    , (Silver, "silver")
+    , (Brass, "brass")
+    , (Metal, "metal")
+    , (Water, "water")
+    , (Empty, "empty")
     ]
 
 -- Item descriptions
@@ -28,13 +91,71 @@ itemDescriptions :: Map Item String
 itemDescriptions =
   Map.fromList
     [ (RustySword, "An old rusty sword. It's not much, but it's better than nothing.")
+    , (SilverSword, "A finely crafted silver sword. It gleams in the light.")
     , (BrassLantern, "A well-crafted brass lantern. It could help light up dark places.")
-    , (WaterFlask, "An empty flask that could be used to carry water.")
+    , (WaterFlask, "A flask filled with clear water.")
+    , (EmptyFlask, "An empty flask that could be used to carry liquids.")
     ]
 
 -- Helper functions
 getItemName :: Item -> String
-getItemName item = Map.findWithDefault "Unnamed Item" item itemNames
+getItemName item =
+  let
+    adjectives = maybe [] Set.toList $ Map.lookup item itemAdjectives
+    adjectiveStrs = map (\adj -> Map.findWithDefault "unknown" adj adjectiveNames) adjectives
+    baseType = Map.findWithDefault Sword item itemTypes
+    baseStr = Map.findWithDefault "unknown" baseType typeNames
+   in
+    unwords $ filter (not . (== "metal")) adjectiveStrs ++ [baseStr]
 
 getItemDescription :: Item -> String
 getItemDescription item = Map.findWithDefault "No description available." item itemDescriptions
+
+getItemType :: Item -> ItemType
+getItemType item = Map.findWithDefault Sword item itemTypes
+
+getItemAdjectives :: Item -> Set Adjective
+getItemAdjectives item = Map.findWithDefault Set.empty item itemAdjectives
+
+-- Convert a string to a possible adjective
+parseAdjective :: String -> Maybe Adjective
+parseAdjective str =
+  let
+    strLower = map toLower str
+   in
+    fst <$> find (\(_, name) -> strLower == name) (Map.toList adjectiveNames)
+
+-- Match an item against a type and list of adjectives
+matchesTypeAndAdjectives :: ItemType -> [Adjective] -> Item -> Bool
+matchesTypeAndAdjectives reqType reqAdjs item =
+  let
+    itemType = getItemType item
+    itemAdjs = getItemAdjectives item
+   in
+    itemType == reqType && all (`Set.member` itemAdjs) reqAdjs
+
+-- Parse and match items based on user input
+-- Returns Either an error message or a list of matching items
+matchItem :: String -> [Item] -> Either String [Item]
+matchItem input items =
+  let
+    words' = words $ map toLower input
+   in
+    if null words'
+      then Left "Please specify an item."
+      else
+        let
+          -- Last word should be the type, rest are potential adjectives
+          (adjStrs, typeStr) = (init words', last words')
+          -- Try to parse all adjectives
+          adjectives = mapMaybe parseAdjective adjStrs
+          -- Try to find matching type
+          matchingType = fst <$> find (\(_, name) -> typeStr == name) (Map.toList typeNames)
+         in
+          case matchingType of
+            Nothing -> Left $ "I don't know what '" ++ typeStr ++ "' is."
+            Just itemType ->
+              let matches = filter (matchesTypeAndAdjectives itemType adjectives) items
+               in case matches of
+                    [] -> Left $ "I can't find any " ++ input ++ " here."
+                    _ -> Right matches
--- src/Main.hs
+++ src/Main.hs
@@ -4,12 +4,11 @@ module Main where
 
 import Commands (Command (..), CommandResult (..))
 import Control.Monad (unless)
-import Data.Char (toLower)
-import Data.List (find, intercalate, isInfixOf)
+import Data.List (intercalate)
 import Data.Map (Map)
 import Data.Map qualified as Map
 import GameState (GameState (..))
-import Items (Item (..), getItemName)
+import Items (Item (..), getItemName, matchItem)
 import Parser (parseCommand)
 import Rooms (
   Room (..),
@@ -23,9 +22,9 @@ import System.IO (hFlush, stdout)
 initialRoomItems :: Map Room [Item]
 initialRoomItems =
   Map.fromList
-    [ (ForestClearing, [RustySword])
+    [ (ForestClearing, [RustySword, SilverSword])
     , (AbandonedCabin, [BrassLantern])
-    , (RiverBank, [WaterFlask])
+    , (RiverBank, [WaterFlask, EmptyFlask])
     ]
 
 -- Initial game state
@@ -45,51 +44,59 @@ setRoomItems :: Room -> [Item] -> GameState -> GameState
 setRoomItems room items state =
   state{roomItems = Map.insert room items (roomItems state)}
 
--- Helper function to find an item by name
-findItem :: String -> [Item] -> Maybe Item
-findItem name = find (\item -> map toLower name `isInfixOf` map toLower (getItemName item))
-
 -- Handle game commands
-handleCommand :: Command -> GameState -> CommandResult
+handleCommand :: Command -> GameState -> IO CommandResult
 handleCommand cmd state = case cmd of
-  Look -> CommandResult state "You look around."
+  Look -> pure $ CommandResult state "You look around."
   Inventory ->
-    CommandResult state $
-      if null (inventory state)
-        then "Your inventory is empty."
-        else "You are carrying: " ++ intercalate ", " (map getItemName $ inventory state)
-  Take itemName -> case findItem itemName (getRoomItems (currentRoom state) state) of
-    Nothing -> CommandResult state $ "There is no " ++ itemName ++ " here."
-    Just item ->
-      let
-        oldItems = getRoomItems (currentRoom state) state
-        newItems = filter (/= item) oldItems
-        newState = setRoomItems (currentRoom state) newItems state
-        finalState = newState{inventory = item : inventory state}
-       in
+    pure $
+      CommandResult state $
+        if null (inventory state)
+          then "Your inventory is empty."
+          else "You are carrying: " ++ intercalate ", " (map getItemName $ inventory state)
+  Take itemName ->
+    case matchItem itemName (getRoomItems (currentRoom state) state) of
+      Left err ->
+        pure $ CommandResult state err
+      Right [item] ->
+        pure $
+          CommandResult
+            ( state
+                { inventory = item : inventory state
+                , roomItems = Map.adjust (filter (/= item)) (currentRoom state) (roomItems state)
+                }
+            )
+            ("You take the " ++ getItemName item ++ ".")
+      Right matches -> do
+        putStrLn $ "Which " ++ itemName ++ " do you mean?"
+        mapM_ (\item -> putStrLn $ "- " ++ getItemName item) matches
+        pure $ CommandResult state "Please be more specific."
+  Drop itemName ->
+    case matchItem itemName (inventory state) of
+      Left err ->
+        pure $ CommandResult state err
+      Right [item] ->
+        pure $
+          CommandResult
+            ( state
+                { inventory = filter (/= item) (inventory state)
+                , roomItems = Map.adjust (item :) (currentRoom state) (roomItems state)
+                }
+            )
+            ("You drop the " ++ getItemName item ++ ".")
+      Right matches -> do
+        putStrLn $ "Which " ++ itemName ++ " do you mean?"
+        mapM_ (\item -> putStrLn $ "- " ++ getItemName item) matches
+        pure $ CommandResult state "Please be more specific."
+  Move dir ->
+    pure $ case Map.lookup dir (getRoomExits (currentRoom state)) of
+      Nothing -> CommandResult state $ "You can't go " ++ show dir ++ "."
+      Just newRoom ->
         CommandResult
-          finalState
-          ("You take the " ++ getItemName item ++ ".")
-  Drop itemName -> case findItem itemName (inventory state) of
-    Nothing -> CommandResult state $ "You don't have a " ++ itemName ++ "."
-    Just item ->
-      let
-        oldItems = getRoomItems (currentRoom state) state
-        newItems = item : oldItems
-        newState = setRoomItems (currentRoom state) newItems state
-        finalState = newState{inventory = filter (/= item) (inventory state)}
-       in
-        CommandResult
-          finalState
-          ("You drop the " ++ getItemName item ++ ".")
-  Move dir -> case Map.lookup dir (getRoomExits (currentRoom state)) of
-    Nothing -> CommandResult state $ "You can't go " ++ show dir ++ "."
-    Just newRoom ->
-      CommandResult
-        (state{currentRoom = newRoom})
-        ("You move " ++ show dir ++ ".")
-  Help -> CommandResult state "Type 'help' for a list of commands."
-  Quit -> CommandResult state "Goodbye!"
+          (state{currentRoom = newRoom})
+          ("You move " ++ show dir ++ ".")
+  Help -> pure $ CommandResult state "Type 'help' for a list of commands."
+  Quit -> pure $ CommandResult state "Goodbye!"
 
 -- Display current game state
 displayState :: GameState -> IO ()
@@ -132,12 +139,13 @@ gameLoop state = do
         putStrLn ""
         putStrLn "Notes:"
         putStrLn "- Articles (a/an/the) are optional"
-        putStrLn "- Items can have multiple words (e.g. 'take rusty sword')"
+        putStrLn "- Items can be referred to by their full name or base name"
+        putStrLn "  (e.g. 'take rusty sword' or just 'take sword')"
         putStrLn "- You can use shorter directions (n/s/e/w)"
         putStrLn "- 'pick up' works the same as 'take'"
         gameLoop state
       _ -> do
-        let result = handleCommand cmd state
+        result <- handleCommand cmd state
         putStrLn $ message result
         displayState (newState result)
         gameLoop (newState result)
